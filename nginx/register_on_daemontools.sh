261d81827967c666069ef32560d4b85c38387e7b
tokubass <tokubass@cpan.org>
2014-10-29 15:38:30 +0900

    register nginx on daemontools

---
+++ nginx/register_on_daemontools.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+svcdir=/service/.nginx
+mkdir -p "$svcdir"
+mkdir -p "$svcdir"/log/main
+
+cat <<EOF
+'EOF' >"$svcdir"/run
+#!/bin/sh
+
+PATH=/usr/local/nginx/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
+CONF=/usr/local/nginx/conf/nginx.conf
+
+exec env - PATH=$PATH nginx -c $CONF  -g "daemon off;" 2>&1
+EOF
+
+cat <<'EOF' >"$svcdir"/log/run
+#!/bin/sh
+exec setuidgid logadmin multilog t s1000000 n100 ./main
+EOF
+
+chmod +x "$svcdir"/run
+chmod +x "$svcdir"/log/run
+chown -R logadmin:logadmin "$svcdir"/log
+chmod -R go+w "$svcdir"/log
